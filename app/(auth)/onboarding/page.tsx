485cb2c17bd024ec31a23762827b7e5228b685ca
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2023-08-06 07:59:04 +0200

    [FIXED] TypeScript errors (onboarding/page.tsx)

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,12 @@
 import AccountProfile from "@/components/forms/AccountProfile";
 import { currentUser } from "@clerk/nextjs";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Page() {
     const user = await currentUser();
+    if (!user) return null
 
-    const userInfo = {}
+    const userInfo = await fetchUser(user.id)
 
     const userData = {
         id: user?.id,
--- lib/utils.ts
+++ lib/utils.ts
@@ -35,9 +35,11 @@ export function formatDateString(dateString: string) {
 export function formatThreadCount(count: number): string {
     if (count === 0) {
         return "No Threads";
+
     } else {
         const threadCount = count.toString().padStart(2, "0");
         const threadWord = count === 1 ? "Thread" : "Threads";
+
         return `${threadCount} ${threadWord}`;
     }
 }

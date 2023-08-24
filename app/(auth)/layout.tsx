aac43ab01b44c5b60a859ae3e79fd5c0bc09c8f0
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2023-08-24 12:59:54 +0200

    [UPDATED] Images and contents

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,6 +5,8 @@ import { Inter } from "next/font/google";
 import '../globals.css';
 import { Metadata } from "next";
 import { dark } from "@clerk/themes";
+import Image from "next/image";
+import { background_two } from "@/public/assets";
 
 export const metadata: Metadata = {
     title: 'Threads',
@@ -18,12 +20,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ClerkProvider appearance={ { baseTheme: dark } }>
             <html lang="en">
                 <body className={`${inter.className} bg-dark-1`}>
-                    {/*create new background for sign in*/}
+                    <Image src={background_two} alt="background" fill/>
                     <div className="w-full flex justify-center items-center min-h-screen">
                         {children}
                     </div>
                 </body>
             </html>
         </ClerkProvider>
-    )
+    );
 }
---
+++ public/assets/index.ts
@@ -0,0 +1,51 @@
+import background_one from "./background-one.jpg";
+import background_two from "./background-two.jpg";
+import community from "./community.svg"
+import create from "./create.svg"
+import deleteIcon from "./delete.svg"
+import edit from "./edit.svg"
+import heart from "./heart.svg"
+import heartFilled from "./heart-filled.svg"
+import heartGray from "./heart-gray.svg"
+import home from "./home.svg"
+import logo from "./logo.svg"
+import logout from "./logout.svg"
+import members from "./members.svg"
+import more from "./more.svg"
+import profile from "./profile.svg"
+import request from "./request.svg"
+import repost from "./repost.svg"
+import search from "./search.svg"
+import share from "./share.svg"
+import reply from "./reply.svg"
+import searchGray from "./search-gray.svg"
+import tag from "./tag.svg"
+import user from "./user.svg"
+import userIcon from"./user-icon.svg"
+
+export {
+    background_one,
+    background_two,
+    community,
+    create,
+    deleteIcon,
+    edit,
+    heart,
+    heartFilled,
+    heartGray,
+    home,
+    logo,
+    logout,
+    members,
+    more,
+    profile,
+    request,
+    repost,
+    search,
+    share,
+    reply,
+    searchGray,
+    tag,
+    user,
+    userIcon,
+}
